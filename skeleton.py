a809ee08969a181b00c40154f5ee8db9d3826afb
Omar <mourra24@gmail.com>
2022-12-25 01:47:29 +0200

    added some print
    added some print to indicate to the user what happened to the site request either read from cache or downloaded or block or not found
    
    Co-Authored-By: Alia Tamer <77108949+AliaMoussa1@users.noreply.github.com>
    Co-Authored-By: Kareem Azab <67200068+KareemAzab21@users.noreply.github.com>

--- skeleton.py
+++ skeleton.py
@@ -24,60 +24,72 @@ while 1:
     urlblocked = urlfile.readlines()
     urlfile.close()
     ############################
-    # Extract the filename from the given message
-    if not (message.split()[1].decode("utf-8") in urlblocked) and message != "":
+    if message:
+        print('#######################\nmessage\n###################\n',message)
         filename = message.split()[1].decode("utf-8").rpartition("/")[2]
-        fileExist = "false"
-        filetouse = "./cache/" + filename
+    # Extract the filename from the given message
+        if not (message.split()[1].decode("utf-8") in urlblocked) :
+            
+            print('#######################\nfilename\n###################\n',filename)
+            fileExist = "false"
+            
+            filetouse = "\\cache\\" + filename
 
-        try:
-            # Check wether the file exist in the cache
-            f = open(filetouse[1:], "rb")
-            outputdata = f.readlines()
-            fileExist = "true"
-            # ProxyServer finds a cache hit and generates a response message
-            tcpCliSock.send(b"HTTP/1.0 200 OK\r\n")
-            tcpCliSock.send(b"Content-Type:text/html\r\n")
-            # Fill in start.
-            print("Read from cache")
-            for line in outputdata:
-                tcpCliSock.send(line)
-            f.close()
-
-        # Error handling for file not found in cache
-        except IOError:
             try:
-                if fileExist == "false":
-                    # Create a socket on the proxyserver
-                    c = socket(AF_INET, SOCK_STREAM)  # Fill in start. # Fill in end.
-                    hostn = message.split()[4].decode("utf-8")
+                # Check wether the file exist in the cache
+                f = open(filetouse[1:], "rb")
+                outputdata = f.readlines()
+                print(f'#######################\nfile exist at {filetouse}\n###################')
+                print("Read from cache")
+                fileExist = "true"
+                # ProxyServer finds a cache hit and generates a response message
+                tcpCliSock.send(b"HTTP/1.0 200 OK\r\n")
+                tcpCliSock.send(b"Content-Type:text/html\r\n")
+                # Fill in start.
+                
+                for line in outputdata:
+                    tcpCliSock.send(line)
+                f.close()
 
-                    # Connect to the socket to port 80
-                    c.connect((hostn, 80))
-                    # Create a temporary file on this socket and ask port 80 for the file requested by the client
-                    fileobjwrite = c.makefile("w", None)
-                    # request
-                    fileobjwrite.write(
-                        "GET " + message.split()[1].decode("utf-8") + " HTTP/1.0\n\n"
-                    )
-                    fileobjwrite.close()
-                    # Read the response into buffer
-                    # read response
-                    fileobj = c.makefile("rb", None)
-                    buff = fileobj.readlines()
-                    # Create a new file in the cache for the requested file.
-                    # Also send the response in the buffer to client socket and the corresponding file in the cache
-                    File = open("./cache/" + filename, "wb+")
-                    for line in buff:
-                        File.write(line)
-                        tcpCliSock.send(line)
-                    File.close()
-                    c.close()
-            except:
-                print("Illegal")
-    else:
-        print("connection blocked")
-        tcpCliSock.close()
+            # Error handling for file not found in cache
+            except IOError:
+                try:
+                    if fileExist == "false":
+                        # Create a socket on the proxyserver
+                        print('#######################\nfile not found\n###################')
+
+                        c = socket(AF_INET, SOCK_STREAM)  # Fill in start. # Fill in end.
+                        hostn = message.split()[4].decode("utf-8")
+
+                        # Connect to the socket to port 80
+                        c.connect((hostn, 80))
+                        # Create a temporary file on this socket and ask port 80 for the file requested by the client
+                        fileobjwrite = c.makefile("w", None)
+                        # request
+                        print('#######################\ncaching....\n###################')
+                        fileobjwrite.write(
+                            "GET " + message.split()[1].decode("utf-8") + " HTTP/1.0\n\n"
+                        )
+                        fileobjwrite.close()
+                        print('#######################\ncaching complete\n###################')
+                        # Read the response into buffer
+                        # read response
+                        fileobj = c.makefile("rb", None)
+                        buff = fileobj.readlines()
+                        # Create a new file in the cache for the requested file.
+                        # Also send the response in the buffer to client socket and the corresponding file in the cache
+                        print('#######################\nsending response from cache\n###################')
+                        File = open("./cache/" + filename, "wb+")
+                        for line in buff:
+                            File.write(line)
+                            tcpCliSock.send(line)
+                        File.close()
+                        c.close()
+                except:
+                    print("Illegal")
+        else:
+            print("\n\n\n\nconnection blocked\n\n\n\n\n")
+            tcpCliSock.close()
 
 else:
     ...
